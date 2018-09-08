282bfb778b9f6a9a98050f994103d0d51441a656
Chris Meller <chris@doesnthaveone.com>
2018-09-08 15:33:54 +0300

    Remove old test code.

--- SCSpendingTransparency.Host/Program.cs
+++ SCSpendingTransparency.Host/Program.cs
@@ -13,7 +13,6 @@ namespace SCSpendingTransparency.Host
 	{
 		static void Main(string[] args)
 		{
-			//Test().ConfigureAwait(false).GetAwaiter().GetResult();
 			Run().ConfigureAwait(false).GetAwaiter().GetResult();
 		}
 
@@ -82,30 +81,6 @@ namespace SCSpendingTransparency.Host
 						}
 					}
 				}
-			}
-		}
-
-		public static async Task Test()
-		{
-			using (var client = new TransparencyClient())
-			{
-				var years = await client.GetAvailableYears();
-				var months = await client.GetAvailableMonths();
-				var agencies = await client.GetAvailableAgencies();
-
-				var judicialDepartment = agencies.First(x => x.Text == "EDUCATION DEPARTMENT");
-				var year2018 = years.First(x => x.Text == "2014 - 15");
-				var monthApril = months.First(x => x.Text == "April");
-
-				var categories = await client.GetMonthCategories(judicialDepartment, year2018, monthApril);
-
-				var categoryContractualServices = categories.First(x => x.Category == "CONTRACTUAL SERVICES");
-
-				var expenses = await client.GetMonthCategoryExpenses(categoryContractualServices);
-
-				var expenseTraining = expenses.First(x => x.Expense == "EDUC TRNG-NON STATE");
-
-				var payments = await client.GetMonthCategoryExpensePayments(expenseTraining);
 			}
 		}
 	}
